df522397ce3fb390d2496656625b871300ce1241
ElJeringas <santiagojss32@gmail.com>
2024-06-18 08:18:02 -0500

    Responsiveness and styles

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -36,3 +36,57 @@ const shadowHeader = () =>{
                        : header.classList.remove('shadow-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+/* emailjs */
+
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_la3425x','template_3jbgmq8', '#contact-form','C2BvruMuKVpNHy-vP')
+    then(() => {
+        console.log('SUCCESS!');
+        contactMessage.textContent = 'Message went sent successfully! ✅'
+
+        setTimeout(()=>{
+            contactMessage.textContent = ''
+        }, 5000)
+
+        contactForm.reset();
+
+    }, (error) => {
+        console.log('FAILED...', error);
+    });
+}
+contactForm.addEventListener('submit', sendEmail);
+
+/* scrollup */
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/* Scroll active link */
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}
+	})
+}
+window.addEventListener('scroll', scrollActive)
